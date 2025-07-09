4e05911c43abc9984cddf9be8a084c1534440ba4
Daniel-A-Klein <52079484+Daniel-A-Klein@users.noreply.github.com>
2025-07-09 09:36:45 -0500

    [Feature] Add argument parsing example

--- src/app_name/app.py
+++ src/app_name/app.py
@@ -1,7 +1,25 @@
 # app_name/app.py
 
+import argparse
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+            prog="app_name",
+            description="Python App Template",
+            epilog="Thanks for using %(prog)s! :)"
+        )
+    
+    parser.add_argument("-s","--str", type = str, help="String argument")
+    parser.add_argument("-i","--int", type = int, help="Integer argument")
+    parser.add_argument("-f","--float", type = float, default=1.0,help="Float argument with default value of 1.0")
+    parser.add_argument("-b","--bool", action="store_true", help="Boolean argument that set to true if set")
+
+    return parser.parse_args()
+
 def main():
     print("Hello Example Python Application!")
+    args = getArgs()
+    print(f"Parsed Args:\n{args}")
 
 if __name__ == "__main__":
     main()
